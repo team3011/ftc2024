825774d4b1420ec2a2c0c3b91694e3749a94e7ea
team3011 <brian.barnes@student.dodea.edu>
2024-01-27 19:01:32 +0100

    test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOppV1.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+import org.firstinspires.ftc.teamcode.robot.subsytems.Shoulder;
+
+@TeleOp(name = "TeleOppV1", group = "Robot")
+@Config
+public class TeleOppV1  extends LinearOpMode {
+    Shoulder shoulder;
+
+    @Override
+    public void runOpMode() {
+        Shoulder shoulder = new Shoulder(
+                hardwareMap.get(DcMotorEx.class,"shoulder"),
+                hardwareMap.get(TouchSensor.class,"shoulderSensor2"));
+
+        waitForStart();
+
+
+        // Scan servo till stop pressed.
+        while(opModeIsActive()){
+            double left_y = gamepad1.left_stick_y;
+            if (Math.abs(left_y)<0.1){
+                left_y = 0;
+            }
+            shoulder.moveShoulderManual(left_y);
+            telemetry.addData("Position", shoulder.getEncoderValue() );
+            telemetry.addData("Sensor", shoulder.getTouchValue());
+            telemetry.addData("stick", left_y);
+            telemetry.update();
+        }
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsytems/Shoulder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsytems/Shoulder.java
@@ -2,26 +2,40 @@ package org.firstinspires.ftc.teamcode.robot.subsytems;
 
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
 public class Shoulder {
-    private MotorEx ShoulderMotor;
+    private DcMotorEx motor;
     private TouchSensor touch;
 
-    public Shoulder(MotorEx motor, TouchSensor methT) {
-        this.ShoulderMotor = motor;
+    public Shoulder(DcMotorEx motor, TouchSensor methT) {
+        this.motor = motor;
         this.touch = methT;
-    //github support lyander-jon dumo
-        this.ShoulderMotor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        this.motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
+    //up is encoder value negative
     public int getEncoderValue() {
-        return this.ShoulderMotor.getCurrentPosition();
+        return this.motor.getCurrentPosition();
     }
 
     public boolean getTouchValue() {
         return this.touch.isPressed();
+    }
+
+    //input > 0 shoulder goes down
+    public void moveShoulderManual(double input){
+        if (this.touch.isPressed() && input > 0){
+            this.motor.setPower(0);
+            this.motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            this.motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        } else {
+            this.motor.setPower(input);
+        }
     }
 
 }
