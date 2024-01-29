7e2b90e2624bba371b753570c97e0a60b7c45baf
team3011 <brian.barnes@student.dodea.edu>
2024-01-29 15:12:02 +0100

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOppV1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOppV1.java
@@ -92,6 +92,8 @@ public class TeleOppV1 extends LinearOpMode {
             arm.moveManual(left_y);
             shoulder.moveManual(right_y);
             lift.moveManual(left_x);
+
+            telemetry.addData("roll",navx.getRoll());
             telemetry.addData("arm Position", arm.getEncoderValue() );
             telemetry.addData("arm Target",arm.getTarget());
             telemetry.addData("shoulder Position", shoulder.getEncoderValue() );
